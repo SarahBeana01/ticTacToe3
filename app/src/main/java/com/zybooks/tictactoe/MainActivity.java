978bee4578f5b2a3e1f2128d4448f4ff997173c9
Kaiden Larsen <kaidenlarsen555@gmail.com>
2023-10-03 21:50:32 -0400

    Added some UI elements. Got started on the controller and model.
    new methods:
    --- Model ---
    ticTacToeGame
    newGame
    getCurrentSymbol
    selectImageButton
    getGameState (currently not in use, not sure if we will need it)
    isGameOver
    anyEmpty
    
    --- Controller ---
    onImageButtonClick
    setButtonImages
    startGame
    onNewGameClick
    
    Problems:
    -You can just click on top of someones spot and change it to yours.
    -Need to increase the background size on the x and o icons, currently looks weird when its placed.
    -Need to implement the textview's functionality. Should be an easy fix inside of onImageButtonClick. Probably just make it so it changes the text at the same time the currentTurn is set.
    -Maybe a font and/or size change on the textView, looks pretty basic right now.
    -Help button not implemented, can just not have one if you want.
    -Appbar not implemented.
    -It would be nice if we could somehow prevent people from clicking more once the game is over.
    -NewGame button is currently a placeholder. We'll move it to the appbar once its made.

--- app/src/main/java/com/zybooks/tictactoe/MainActivity.java
+++ app/src/main/java/com/zybooks/tictactoe/MainActivity.java
@@ -1,45 +1,123 @@
 package com.zybooks.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
+import android.annotation.SuppressLint;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.GridLayout;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private ticTacToeGame mGame;
-    private GridLayout mSquareGrid;
-    private int mSquareImageX;
-    private int mSquareImageO;
+    private GridLayout mGridView;
+    public Character currentTurn = 'X';
+    Drawable xIcon;
+    Drawable oIcon;
 
+    @SuppressLint("UseCompatLoadingForDrawables")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Drawable myX = getResources().getDrawable(R.drawable.myX);
 
-        mSquareImageX = findViewById(R.);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mSquareGrid = findViewById(R.id.square_grid);
 
-        mLightOnColor = ContextCompat.getColor(this, R.color.yellow);
-        mLightOffColor = ContextCompat.getColor(this, R.color.black);
-
+        xIcon = getDrawable(R.mipmap.ticatac_xicon);
+        oIcon = getDrawable(R.mipmap.ticatac_oicon);
+        mGridView = findViewById(R.id.game_grid);
         mGame = new ticTacToeGame();
+
+        for (int buttonIndex = 0; buttonIndex < mGridView.getChildCount(); buttonIndex++) {
+            //This is a for loop that iterates through each cell in the mLightGrid getting the count with getChildCount().
+
+            ImageButton gridButton = (ImageButton) mGridView.getChildAt(buttonIndex);
+            //This making it so the button at the current index is gridButton, this will be used on the next line
+
+            gridButton.setOnClickListener(this::onImageButtonClick);
+            //This is assigning an onClickListener to gridButton, its basically saying that when you click the gridButton I want onLightButtonClick to be called
+        }
+
+        //Can probably omit this part, we don't need to do any restoration.
+        /*
         if (savedInstanceState == null) {
             startGame();
         } else {
             String gameState = savedInstanceState.getString(GAME_STATE);
-            mGame.setState(gameState);
+            mGame.setGameState(gameState);
             setButtonColors();
         }
+        */
     }
+
+    private void onImageButtonClick(View view) {
+        //This is called whenever you click a light-button
+
+        // Find the button's row and col
+        int buttonIndex = mGridView.indexOfChild(view);
+        //This gets the index of the child using the indexOfChild method
+
+        int row = buttonIndex / ticTacToeGame.GRID_SIZE;
+        //This finds the row by dividing the index by the grid size, I would have never thought of it but good on them
+
+        int col = buttonIndex % ticTacToeGame.GRID_SIZE;
+        //This finds the column using the remainder operator, I'm not going to pretend like I understand it
+
+        mGame.selectImageButton(row, col, currentTurn);
+        //This calls the selectLight method from LightsOutGame on the current cell, this method is responsible for inverting the lights
+
+        setButtonImages();
+        //without this the buttons would be invisible
+
+        //This swaps the turn to the next person
+        if(currentTurn == 'X') {
+            currentTurn = 'O';
+        } else {
+            currentTurn = 'X';
+        }
+
+        // Congratulate the user if the game is over
+        if (mGame.isGameOver()) {
+            //If isGameOver returns true you'll get a congrats toast
+
+
+            Toast.makeText(this, mGame.whoWon, Toast.LENGTH_SHORT).show();
+            //Here's the toast code, it uses the congrats string, with a short length
+        }
+    }
+
+    //This is our method that will update the buttons backgrounds to the appropriate image/color.
+    private void setButtonImages() {
+        for (int buttonIndex = 0; buttonIndex < mGridView.getChildCount(); buttonIndex++) {
+            //This iterates through each child in the gridview, it got the count with getChildCount
+
+            ImageButton gridButton = (ImageButton) mGridView.getChildAt(buttonIndex);
+            //as its iterating through each child it is making it gridButton
+
+            int row = buttonIndex / ticTacToeGame.GRID_SIZE;
+            int col = buttonIndex % ticTacToeGame.GRID_SIZE;
+
+            if (mGame.getCurrentSymbol(row, col) == 'X') {
+                gridButton.setBackground(xIcon);
+                gridButton.setContentDescription(Integer.toString(R.string.letter_x));
+            }
+            else if (mGame.getCurrentSymbol(row, col) == 'O') {
+                gridButton.setBackground(oIcon);
+                gridButton.setContentDescription(Integer.toString(R.string.letter_o));
+            } else {
+                gridButton.setBackgroundColor(getColor(R.color.white));
+            }
+        }
+    }
+     private void startGame() {
+        mGame.newGame();
+        setButtonImages();
+     }
+
     public void onNewGameClick(View view) {
-        // Creates public function called onNewGameClick
+        // Creates public method called onNewGameClick
         startGame();
         // Calls function startGame
     }
--- app/src/main/java/com/zybooks/tictactoe/ticTacToeGame.java
+++ app/src/main/java/com/zybooks/tictactoe/ticTacToeGame.java
@@ -1,14 +1,109 @@
 package com.zybooks.tictactoe;
 
-public class ticTacToeGame() {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ticTacToeGame extends AppCompatActivity {
     public static final int GRID_SIZE = 3;
-    private final boolean[][] mSquareGrid;
-    mSquareGrid = new boolean[GRID_SIZE][GRID_SIZE];
-}
+    private final char[][] mGameGrid;
+    public String whoWon;
+
+
+
+
+    //This will be used to create an instance of this class over in other places.
+    public ticTacToeGame() {
+        mGameGrid = new char[GRID_SIZE][GRID_SIZE];
+    }
+
+    /*
     public void newGame() {
         for (int row = 0; row < GRID_SIZE; row++) {
             for (int col = 0; col < GRID_SIZE; col++){
-                mSquareGrid[row][col] =
+                mImageButton = mGridView.getChildAt()
             }
         }
     }
+    */
+
+    //testing out an alternate newGame method
+    public void newGame() {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++){
+                mGameGrid[row][col] = 'W'; //using 'W' to resemble white, or an empty ImageButton
+            }
+        }
+    }
+
+    public Character getCurrentSymbol(int row, int col) {
+        return mGameGrid[row][col];
+    }
+
+    public void selectImageButton(int row, int col, char currentTurn) {
+        //This is what inverts the lights when you tap them, it takes the coordinates and passes them along though the method
+        mGameGrid[row][col] = currentTurn;
+    }
+
+    /*  Not in use yet, possibly not needed
+    public String getGameState() {
+        //This gets the current state of all the ImageButtons
+
+        StringBuilder boardString = new StringBuilder();
+        //Here we are creating a new instance of the StringBuilder class named boardString. This will hold the state of the lights.
+
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+
+                char value = mGameGrid[row][col];
+
+                boardString.append(value);
+                //Here it just appends the character to the end of boardString
+            }
+        }
+
+        return boardString.toString();
+        //This is just returning the string we just built, it will be used to recover after the device is rotated primarily
+    }
+*/
+    public boolean isGameOver() {
+
+        for(int index = 0; index < GRID_SIZE; index++) {
+            //These first two if statements should iterate through every row and column respectively looking for any non diagonal matches.
+            //Making sure to include the "&& mGameGrid[][] != 'W'" at the end of each if statement to make sure it doesn't declare white as the winner.
+            if(mGameGrid[index][0] == mGameGrid[index][1] && mGameGrid[index][1] == mGameGrid[index][2] && mGameGrid[index][0] != 'W') {
+                whoWon = mGameGrid[index][0] + " won the game";
+                return true;
+            }
+            if(mGameGrid[0][index] == mGameGrid[1][index] && mGameGrid[1][index] == mGameGrid[2][index] && mGameGrid[0][index] != 'W') {
+                whoWon = mGameGrid[0][index] + " won the game";
+                return true;
+            }
+            //these next two should be checking the diagonals for matches.
+            if(mGameGrid[0][0] == mGameGrid[1][1] && mGameGrid[1][1] == mGameGrid[2][2] && mGameGrid[0][0] != 'W') {
+                whoWon = mGameGrid[0][0] + " won the game";
+                return true;
+            }
+            if(mGameGrid[0][2] == mGameGrid[1][1] && mGameGrid[1][1] == mGameGrid[2][0] && mGameGrid[0][2] != 'W') {
+                whoWon = (mGameGrid[0][2]) + " won the game";
+                return true;
+            }
+        }
+        if (anyEmpty()) {
+            whoWon = "CAT";
+            return true;
+        }
+        return false;
+    }
+
+    public boolean anyEmpty() {
+        for (int row = 0; row < GRID_SIZE; row++) {
+            for (int col = 0; col < GRID_SIZE; col++) {
+
+                if(mGameGrid[row][col] == 'W') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+}
